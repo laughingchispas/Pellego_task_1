273c0ea33b280c701d7a3e164c8155eb3fc8bbf7
Nicole Miller <laughingchispas@gmail.com>
2016-06-16 19:38:13 +0200

    Incorporate comments June 16th - Angular

--- scripts/angularApp.js
+++ scripts/angularApp.js
@@ -1,10 +1,15 @@
 angular.module('angularApp', [])
-    .controller('card_controller', function($scope) {
+    .controller('CardController', function($scope) {
     $scope.movieCards = [
         {movieTitle: 'Casablanca', movieDirector: 'Curtiz'},
         {movieTitle: 'Citizen Kane', movieDirector: 'Welles'},
         {movieTitle: 'Psycho', movieDirector: 'Hitchcock'},
         {movieTitle: 'Godfather', movieDirector: 'Coppola'},
-        {movieTitle: 'Back to the Future', movieDirector: 'Zemeckis'}
+        {movieTitle: 'Back to the Future', movieDirector: 'Zemeckis'},
+        {movieTitle: 'Casablanca', movieDirector: 'Curtiz'},
+                {movieTitle: 'Citizen Kane', movieDirector: 'Welles'},
+                {movieTitle: 'Psycho', movieDirector: 'Hitchcock'},
+                {movieTitle: 'Godfather', movieDirector: 'Coppola'},
+                {movieTitle: 'Back to the Future', movieDirector: 'Zemeckis'}
     ];
 });
