51242dc1f0a46f7833045b3282f06540af366f02
Nicole Miller <laughingchispas@gmail.com>
2016-06-24 19:28:07 +0200

    Two additional controllers + CSS and HTML changes

--- scripts/angularApp.js
+++ scripts/angularApp.js
@@ -1,15 +1,43 @@
-angular.module('angularApp', [])
-    .controller('CardController', function($scope) {
+var myApp = angular.module('angularApp', []);
+
+
+
+myApp.controller('navController', function($scope) {
+    $scope.navCards = [
+            {page: "home"},
+            {page: "movies"},
+            {page: "about us"},
+            {page: "links"},
+            {page: "contacts"}
+        ]
+    });
+
+myApp.controller('CardController', function($scope) {
     $scope.movieCards = [
-        {movieTitle: 'Casablanca', movieDirector: 'Curtiz'},
-        {movieTitle: 'Citizen Kane', movieDirector: 'Welles'},
-        {movieTitle: 'Psycho', movieDirector: 'Hitchcock'},
-        {movieTitle: 'Godfather', movieDirector: 'Coppola'},
-        {movieTitle: 'Back to the Future', movieDirector: 'Zemeckis'},
-        {movieTitle: 'Casablanca', movieDirector: 'Curtiz'},
-                {movieTitle: 'Citizen Kane', movieDirector: 'Welles'},
-                {movieTitle: 'Psycho', movieDirector: 'Hitchcock'},
-                {movieTitle: 'Godfather', movieDirector: 'Coppola'},
-                {movieTitle: 'Back to the Future', movieDirector: 'Zemeckis'}
-    ];
-});
+        {title: 'Casablanca', director: 'Curtiz', genre: "drama"},
+        {title: 'Citizen Kane', director: 'Welles', genre: "adventure"},
+        {title: 'Psycho', director: 'Hitchcock', genre: "horror"},
+        {title: 'Godfather', director: 'Coppola', genre: "comedy"},
+        {title: 'Back to the Future', director: 'Zemeckis', genre: "science fiction"},
+        {title: 'Casablanca', director: 'Curtiz', genre: "drama"},
+        {title: 'Citizen Kane', director: 'Welles', genre: "drama"},
+        {title: 'Psycho', director: 'Hitchcock', genre: "drama"},
+        {title: 'Godfather', director: 'Coppola', genre: "drama"},
+        {title: 'Back to the Future', director: 'Zemeckis', genre: "drama"}
+    ]
+    });
+
+
+myApp.controller('GenreController', function($scope) {
+    $scope.genreCards = [
+            {genre: "drama"},
+            {genre: "adventure"},
+            {genre: "horror"},
+            {genre: "comedy"},
+            {genre: "science fiction"}
+        ]
+    });
+
+
+
+
